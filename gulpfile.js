c3252ad7ec8aa6f550d2a14587cda73a72cedd1f
Jordan Schalm <jordan.schalm@gmail.com>
2015-10-23 11:00:57 -0700

    Finish gulpfile with dev & production settings Add conditional to layout.html to switch between dev & production script/css settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var glob = require('glob');
 var minifyCSS = require('gulp-minify-css');
 var concat = require('gulp-concat');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
 var gulpif = require('gulp-if');
 var argv = require('minimist')(process.argv.slice(2));
 
@@ -16,9 +21,62 @@ var dev = typeof(argv.d) !== 'undefined';
 	files. Then, places file(s) in public/css/dist
 */
 gulp.task('sass', function() {
-	return gulp.src('./public/css/src/**/*.scss')
+	return gulp.src(['./public/css/src/**/*.scss'])
 		.pipe(sass())
 		.pipe(gulpif(!dev, minifyCSS()))
 		.pipe(gulpif(!dev, concat('main.min.css')))
 		.pipe(gulp.dest('./public/css/dist'));
 });
+
+/*
+	Finds .js files in public/js/src and resoves 'require' 
+	dependencies with browserify. If -d is not specified, 
+	uglifies and concatenates all source files. Then places
+	file(s) in public/js/dist
+*/
+gulp.task('browserify', function(cb) {
+	glob('./public/js/src/*.js', function(err, files) {
+		var browserifier = browserify();
+		files.forEach(function(file) {
+			browserifier.add(file)
+			if(dev) {
+				// Individually process each file
+				_processor(browserifier.bundle(), _filename(file));
+				// Reset browserifier
+				browserifier = browserify();
+			}
+		});
+		if(!dev) {
+			// Process all files at once
+			_processor(browserifier.bundle(), 'main.min.js');
+		}
+	});
+});
+
+/*
+	Gulp processor function to compile and browserify
+	individual files. If -d is not specified, uglifies 
+	and concatenates all source files. Places output 
+	files in ./public/js/dist
+
+	bundle - a browserify bundle
+	dist_name - specifies name of output file
+*/
+function _processor(bundle, dist_name) {
+	bundle
+		.pipe(source(dist_name))
+		.pipe(gulpif(!dev, buffer()))
+		.pipe(gulpif(!dev, uglify()))
+		.pipe(gulp.dest('./public/js/dist'));
+}
+
+/*
+	Given an absolute or relative path of a javascript
+	file, returns the name by removing parent directories
+	from the path string.
+
+	file_path - path to file
+*/
+function _filename(file_path) {
+	return file_path.match(/\w+\.js/)[0];
+}
---
+++ public/js/src/source.js
@@ -0,0 +1,5 @@
+var x = require('path');
+
+for(var i = 0; i < 1000; i++) {
+	console.log(i);
+}
