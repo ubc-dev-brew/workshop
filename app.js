c9aa3ab74bac3ae6edf3ab7303dd7a883eb169ed
Arabelle <arabelle310@yahoo.ca>
2015-10-24 10:23:26 -0700

    Added connection to mongodb using mongoose.

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ console.log(config.message);
 // Resolve paths to client files.
 var client_files = path.resolve(__dirname, './client/');
 
+// Set a 30 second connection timeout for mongodb
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+
 // Register our templating engine
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
@@ -23,12 +27,23 @@ app.set('views', __dirname + '/views');
 // Express middleware to serve static files
 app.use(express.static('public'));
 
-
 // Get routes from routes/main.js
 require('./routes/main.js')(express,app);
 
 // Set a port to listen to for the server
 app.set('port', (process.env.PORT || 5000));
+
+// Connect to MongoDB
+var mongodbUri = 'mongodb://admin:devbrewadmin@ds043324.mongolab.com:43324/devbrew-workshop';
+mongoose.connect(mongodbUri, options);
+
+var conn = mongoose.connection;
+
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+conn.once('open', function callback () {
+	// Application...
+});
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port',
