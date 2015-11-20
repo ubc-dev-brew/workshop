65e29f61ff8e524ed830d75bc4de4be9cd6b09a8
Arabelle <arabelle310@yahoo.ca>
2015-11-19 16:53:22 -0800

    Add first and last name to sign up, clean up api javascript

--- routes/api.js
+++ routes/api.js
@@ -1,57 +1,6 @@
 module.exports = function(express, app, middleware, multipart, cloudinary, User, Post) {
 	var router = express.Router();
 	
-	router.post('/profile', function(req, res) {
-		var form = new multipart.Form();
-		var userModel = {};
-		
-		form.on('error', function(err) {
-			console.log("Error occurred while uploading a profile picture: " + err.stack);
-			// TODO handle error condition properly (ajax response to form)
-		});
-		
-		form.on('part', function(part) {
-			if(!part.filename) {
-				// The part is a form field, not a file
-				console.dir(part);
-				part.resume();
-			}
-			if(part.filename) {
-				// The part is a file, stream to Cloudinary
-				var stream = cloudinary.uploader.upload_stream(function(result) {
-					userModel.profilePictureUrl = result.url;
-				});
-				part.pipe(stream);
-				part.resume();
-			}
-			part.on('error', function(err) {
-				console.log("Error occurred while streaming a profile picture part: " + err.stack);
-				// TODO handle error condition properly (ajax response to form)
-			});
-		});
-		
-		form.on('field', function(name, value) {
-			userModel[name] = value;
-		});
-		
-		form.parse(req, function(err, fields, files) {
-			if(err) {
-				console.log("Error occurred while parsing a multi-part form.\nHTTP POST /profile\n" + err.stack);
-				// TODO handle error condition properly (ajax response to form)
-			}
-			var newUser = new User(userModel);
-			newUser.save(function(err, result) {
-				if(err) {
-					console.log("Error occurred while saving a new user to MongoDB: " + err.stack);
-					// TODO handle error condition properly (ajax response to form)
-				}
-				console.log("Added new user. " + JSON.stringify(result));
-			});
-		});
-		
-		res.render('feed');
-	});
-    
     // ================================================
     // UPDATE USER INFORMATION
 	// ================================================
