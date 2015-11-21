0c3e0d33632b12977a6f0532d5a218d05dc62a88
Elaine Feng <eleanor52@live.ca>
2015-11-21 14:53:35 -0800

    finished signup page

--- public/js/dist/source.js
+++ public/js/dist/source.js
@@ -7,7 +7,6 @@ function showEditForm() {
     var userSummary = document.getElementById('userSummary').style.display= "none";
 };
 
-
 function showUserSummary() {
     var editForm = document.getElementById('editForm').style.display= "none";;
     var userSummary = document.getElementById('userSummary').style.display= "block";
